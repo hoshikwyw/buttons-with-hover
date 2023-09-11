0b29bef0feb7704cabff5e66069ffb234e651e19
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-09-11 22:17:31 +0630

    color change

--- src/001/NormalBtn.jsx
+++ src/001/NormalBtn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NormalBtn = () => {
   return (
     <div>
-    <button className=" text-[#B29079] border border-[#C1B6A4] px-5 py-1 rounded hover:bg-[#C1B6A4] text-lg font-semibold hover:text-white transition-all duration-200">
+    <button className=" text-[#B29079] border border-[#C1B6A4] px-5 py-1 rounded hover:bg-[#405f8d] text-lg font-semibold hover:text-white transition-all duration-200">
       Normal  !
     </button>
     </div>
--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import NormalMiddle from "./006/NormalMiddle";
 
 const App = () => {
   return (
-    <div className="  bg-[#e0dfdfa9] h-screen">
+    <div className="  bg-[#1d1c1cd5] h-screen">
       <h2 className=" brandy uppercase font-serif font-bold text-2xl text-center py-4 tracking-widest">
         Button with hover effects
       </h2>
       <div className=" mx-5 my-3">
-        <h2 className=" font-semibold tracking-widest text-xl underline text-[#61614c]">Normal Hover Effects</h2>
+        <h2 className=" font-semibold tracking-widest text-xl underline text-[#a5a550]">Normal Hover Effects</h2>
         <div className=" flex gap-5 mt-3 mx-3">
           <NormalBtn />
           <NormalLeft />
